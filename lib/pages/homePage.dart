b4ec614702842f22cce4d5b7f9f48bcb553d7e8a
Leonardo Wille <leonardo.wille@cwi.com.br>
2020-02-23 16:06:18 -0300

    feat: add bottom navigation

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -13,6 +13,8 @@ class _HomePageState extends State<HomePage> {
   List<Pokemon> _pokemons;
   List<Pokemon> _pokemonsFiltered;
   TextEditingController _filterInputController = TextEditingController();
+  int _currentIndex = 0;
+  String _appBarTitle = "Pokemon";
 
   _filterPokemonList() {
     if (_pokemons != null && _pokemons.isNotEmpty) {
@@ -32,6 +34,12 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  _updateIndex(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   _getPokemons() async {
     _pokemons = await PokemonApi().getPokemons();
     _pokemonsFiltered = _pokemons;
@@ -44,15 +52,123 @@ class _HomePageState extends State<HomePage> {
       return Loader();
     }
 
-    return Padding(
-      padding: const EdgeInsets.only(top: 100),
-      child: Container(
-        child: ListView.builder(
-          itemCount: _pokemonsFiltered.length,
-          itemBuilder: (context, index) {
-            return PokemonListItem(pokemon: _pokemonsFiltered[index]);
-          },
-        ),
+    switch (_currentIndex) {
+      case 0:
+        return Padding(
+          padding: const EdgeInsets.only(top: 100),
+          child: Container(
+            child: ListView.builder(
+              itemCount: _pokemonsFiltered.length,
+              itemBuilder: (context, index) {
+                return PokemonListItem(pokemon: _pokemonsFiltered[index]);
+              },
+            ),
+          ),
+        );
+      default:
+        return Center(child: Text("TODO"));
+    }
+  }
+
+  Widget _appBar() {
+    return Container(
+      height: 100,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Colors.lightBlueAccent.withOpacity(0.3),
+          Colors.lightGreenAccent.withOpacity(0.3)
+        ]),
+      ),
+      child: Column(
+        children: <Widget>[
+          Container(height: 16),
+          Text(
+            _appBarTitle,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+          ),
+          Container(height: 10),
+          Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16),
+            child: Container(
+              height: 36,
+              decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.3),
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              child: TextField(
+                autofocus: false,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.black54,
+                    ),
+                    suffixIcon: Icon(
+                      Icons.mic,
+                      color: Colors.black54,
+                    ),
+                    hintText: "Search",
+                    border: InputBorder.none),
+                controller: _filterInputController,
+              ),
+            ),
+          ),
+          Container(height: 10),
+          Container(
+            height: 4,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Colors.lightBlueAccent,
+                Colors.lightGreenAccent,
+              ]),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _bottomNavigationBar() {
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [Colors.lightBlueAccent, Colors.lightGreenAccent]),
+      ),
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 4,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Colors.lightBlueAccent,
+                Colors.lightGreenAccent,
+              ]),
+            ),
+          ),
+          Theme(
+            data: Theme.of(context).copyWith(
+                primaryColor: Colors.black,
+                textTheme: Theme.of(context)
+                    .textTheme
+                    .copyWith(caption: new TextStyle(color: Colors.grey))),
+            child: BottomNavigationBar(
+              backgroundColor: Colors.white.withOpacity(0.7),
+              currentIndex: _currentIndex,
+              onTap: _updateIndex,
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.category), title: Text("Pokemon")),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.add_box), title: Text("Moves")),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.assignment_returned),
+                    title: Text("Items")),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -60,7 +176,6 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-
     _filterInputController.addListener(_filterPokemonList);
   }
 
@@ -74,109 +189,12 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      bottomNavigationBar: Container(
-        height: 60,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [Colors.lightBlueAccent, Colors.lightGreenAccent]),
-        ),
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 4,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [
-                  Colors.lightBlueAccent,
-                  Colors.lightGreenAccent,
-                ]),
-              ),
-            ),
-            Theme(
-              data: Theme.of(context).copyWith(
-                  primaryColor: Colors.black,
-                  textTheme: Theme.of(context)
-                      .textTheme
-                      .copyWith(caption: new TextStyle(color: Colors.grey))),
-              child: BottomNavigationBar(
-                backgroundColor: Colors.white.withOpacity(0.7),
-                items: <BottomNavigationBarItem>[
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.category), title: Text("Pokemon")),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.add_box), title: Text("Moves")),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.assignment_returned),
-                      title: Text("Items")),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+      bottomNavigationBar: _bottomNavigationBar(),
       body: SafeArea(
         child: Stack(
           children: <Widget>[
-            Container(
-              height: 100,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [
-                  Colors.lightBlueAccent.withOpacity(0.3),
-                  Colors.lightGreenAccent.withOpacity(0.3)
-                ]),
-              ),
-              child: Column(
-                children: <Widget>[
-                  Container(height: 16),
-                  Text(
-                    "Pokemon",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-                  ),
-                  Container(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16, right: 16),
-                    child: Container(
-                      height: 36,
-                      decoration: BoxDecoration(
-                          color: Colors.grey.withOpacity(0.3),
-                          borderRadius: BorderRadius.all(Radius.circular(20))),
-                      child: TextField(
-                        autofocus: false,
-                        decoration: InputDecoration(
-                            prefixIcon: Icon(
-                              Icons.search,
-                              color: Colors.black54,
-                            ),
-                            suffixIcon: Icon(
-                              Icons.mic,
-                              color: Colors.black54,
-                            ),
-                            hintText: "Search",
-                            border: InputBorder.none),
-                        controller: _filterInputController,
-                      ),
-                    ),
-                  ),
-                  Container(height: 10),
-                  Container(
-                    height: 4,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(colors: [
-                        Colors.lightBlueAccent,
-                        Colors.lightGreenAccent,
-                      ]),
-                    ),
-                  )
-                ],
-              ),
-            ),
+            _appBar(),
             _mainContainer(),
-//            Align(
-//              alignment: Alignment.bottomCenter,
-//              child:
-//            ),
           ],
         ),
       ),
